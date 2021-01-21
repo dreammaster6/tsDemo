12b85cabcdcb7465078ecdf0a595fe3c42b7dd49
bss_gis_dengyue <dengyue@asiainfo.com>
2021-01-21 09:49:39 +0800

    tsDemo提交到git仓库

---
+++ 数据类型/index.js
@@ -0,0 +1,21 @@
+// 布尔类型
+var flag = false;
+flag = true;
+// 数值类型
+var num = 12;
+num = 12.3;
+// 字符串类型
+var str = 'str';
+str = 12 + 's';
+// undefined类型
+// null类型
+// 数组类型
+// 第一种定义方式
+var array_1 = [1, 2, 3];
+var array_4 = [1, 2, 3];
+// 第二种定义方式
+var array_2 = ['1', '2', '3'];
+var array_3 = [{ 1: 23 }];
+console.log(array_1 == array_4);
+var a = array_4;
+console.log(a == array_4);
---
+++ 数据类型/index.ts
@@ -0,0 +1,28 @@
+// 布尔类型
+var flag : boolean = false
+flag = true
+
+// 数值类型
+var num : number = 12
+num = 12.3
+
+// 字符串类型
+
+var str : string = 'str'
+str = 12 + 's'
+
+// undefined类型
+
+// null类型
+
+// 数组类型
+// 第一种定义方式
+var array_1 : number[] = [1,2,3]
+var array_4 : Object[] = [1,2,3]
+// 第二种定义方式
+var array_2 : Array<String> = ['1','2','3']
+var array_3 : Array<Object> = [{1:23}]
+
+// 元组类型(数组的一种)
+
+var array_tuple : [string,number,boolean] = [1,'2',2]
